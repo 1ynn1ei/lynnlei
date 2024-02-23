3a23e1e35f80651e1b618f69a20de2e1aa60e75f
Brooke Leichtle <brooke.leichtle@gmail.com>
2024-02-23 23:32:10 +0100

    rendering a markdown

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+use maud::{html, Render};
+mod template;
+mod markdown;
+
+use crate::markdown::Markdown;
+fn main() {
+    let content = fs::read_to_string("blog/test.md").unwrap();
+    let markdown_content = Markdown(content);
+    let markup = template::body(Some(String::from("test")),
+    markdown_content.render()
+    );
+    println!("{}", markup.into_string());
+}
